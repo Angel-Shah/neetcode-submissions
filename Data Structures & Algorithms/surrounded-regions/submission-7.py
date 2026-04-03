d697c3d694971c58ebda5c5e0dd4691cf0329011
Angel Shah <angel.shah2468@gmail.com>
2026-04-03 00:32:06 -0700

    Add: surrounded-regions - submission-7

---
+++ Data Structures & Algorithms/surrounded-regions/submission-7.py
@@ -0,0 +1,31 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        rows, cols = len(board),len(board[0])
+
+        def dfs(r,c,currSet):
+            if(r >= 0 and r < rows and
+            c >= 0 and c < cols and
+            (r,c) not in currSet):
+                if board[r][c] == 'O':
+                    currSet.add((r,c))
+                    dirs = [(1,0),(-1,0),(0,1),(0,-1)]
+                    for dr,dc in dirs:
+                        nr,nc = r + dr , c + dc
+                        dfs(nr,nc,currSet)
+            return currSet
+
+        def isSurrounded(islandSet):
+            for r,c in islandSet:
+                if (r == 0 or r == rows-1 or
+                    c == 0 or c == cols-1):
+                    return False
+            return True
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == 'O':
+                    island = dfs(r,c,set())
+                    if isSurrounded(island):
+                        for x,y in island:
+                            board[x][y] = 'X'
+
+        
