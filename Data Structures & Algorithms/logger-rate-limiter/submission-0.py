e2345d933b558b73aff7706196e6eff914fdb9b8
Angel Shah <angel.shah2468@gmail.com>
2026-05-16 15:41:10 -0700

    Add: logger-rate-limiter - submission-0

---
+++ Data Structures & Algorithms/logger-rate-limiter/submission-0.py
@@ -0,0 +1,20 @@
+class Logger:
+
+    def __init__(self):
+        self.nextAllowed = {}
+        
+
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+        if message not in self.nextAllowed:
+            self.nextAllowed[message] = timestamp + 10
+            return True
+        if timestamp >= self.nextAllowed[message]:
+            self.nextAllowed[message] = timestamp + 10
+            return True
+        else:
+            return False
+
+
+# Your Logger object will be instantiated and called as such:
+# obj = Logger()
+# param_1 = obj.shouldPrintMessage(timestamp,message)
