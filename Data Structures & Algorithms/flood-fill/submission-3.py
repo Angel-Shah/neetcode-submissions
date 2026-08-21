3a342e11df03d2edeff90156fe1ff735d1844b9d
Angel Shah <angel.shah2468@gmail.com>
2026-08-21 16:34:31 -0700

    Add: flood-fill - submission-3

---
+++ Data Structures & Algorithms/flood-fill/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        src_color = image[sr][sc]
+        rows,cols = len(image), len(image[0])
+        # perform a dfs starting from image[sr][sc]
+        visited = set()
+        def dfs(r,c):
+            if min(r,c) < 0 or r == rows or c == cols or (r,c) in visited or image[r][c] != src_color:
+                return
+            
+            image[r][c] = color
+            visited.add((r,c))
+
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+
+            visited.remove((r,c))
+        
+        dfs(sr,sc)
+        return image
+
